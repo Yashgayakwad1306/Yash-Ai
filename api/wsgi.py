33b643409860844f71ff7d00e2e171d8e468ad84
Yash <ygayakwad1306@gmail.com>
2026-06-12 19:12:14 +0530

    Fix wsgi.py configuration

--- api/wsgi.py
+++ api/wsgi.py
@@ -1,10 +1,7 @@
-# api/wsgi.py
 import os
 from django.core.wsgi import get_wsgi_application
 
-# Point to your main aichat folder settings
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'aichat.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'aichat.aichat.settings')
 
 application = get_wsgi_application()
-
 app = application
